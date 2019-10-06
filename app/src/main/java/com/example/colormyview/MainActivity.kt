5d42bc0c207171f19b24b8d9c0d5e4dc54c67f10
Alex Herrera <54720239+White-Hat-Blue-Feather@users.noreply.github.com>
2019-10-06 15:06:38 -0700

    Changes: > added 3 buttons (red, blue, green)

--- app/src/main/java/com/example/colormyview/MainActivity.kt
+++ app/src/main/java/com/example/colormyview/MainActivity.kt
@@ -16,7 +16,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setListeners() {
-        val clickableViews: List<View> = listOf(textView_1, textView_2, textView_3, textView_4, textView_5)
+        val clickableViews: List<View> = listOf(textView_1, textView_2, textView_3,
+                                                textView_4, textView_5, button_1,
+                                                button_2, button_3)
         for (item in clickableViews){
             item.setOnClickListener { changeColor(it) }
         }
@@ -30,6 +32,10 @@ class MainActivity : AppCompatActivity() {
             R.id.textView_3 -> view.setBackgroundResource(android.R.color.holo_orange_light)
             R.id.textView_4 -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.textView_5 -> view.setBackgroundResource(android.R.color.holo_red_light)
+
+            R.id.button_1 -> view.setBackgroundResource(R.color.my_green)
+            R.id.button_2 -> view.setBackgroundResource(R.color.my_red)
+            R.id.button_3 -> view.setBackgroundResource(R.color.my_blue)
 
             else -> view.setBackgroundColor(Color.DKGRAY)
         }
