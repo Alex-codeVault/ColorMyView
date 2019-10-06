5eefa33e947c77be356ab6a5c6ea7befd28c66b3
Alex Herrera <54720239+White-Hat-Blue-Feather@users.noreply.github.com>
2019-10-06 13:45:29 -0700

    Changes: > added 6 textView > dimens resources (margins) > style resources (for textViews) > string resources (for textViews) > added a MainActivity function, name: setListeners()

--- app/src/main/java/com/example/colormyview/MainActivity.kt
+++ app/src/main/java/com/example/colormyview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.colormyview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setListeners()
+    }
+
+    private fun setListeners() {
+        val clickableViews: List<View> = listOf(textView_1, textView_2, textView_3, textView_4, textView_5)
     }
 }
